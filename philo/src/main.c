65d4e1ceb057f3ea61fc6cf5895b55ca5bea5aae
dande-je <dande-je@student.42sp.org.br>
2024-08-16 17:23:26 -0300

    Feat: Init the basic of the code, makefile and the main.c.  Changes to be committed: 	modified:   .gitignore 	new file:   philo/.gitignore 	new file:   philo/Makefile 	new file:   philo/src/main.c

---
+++ philo/src/main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 17:17:10 by dande-je          #+#    #+#             */
+/*   Updated: 2024/08/16 17:20:36 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	printf("Init Philo, %d argc | %s argv\n", argc, *argv);
+	exit(EXIT_SUCCESS);
+}
