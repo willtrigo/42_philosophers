bd23c45b235f00c4fcae904879fcf3306fd64436
willtrigo <willtrigo@gmail.com>
2024-12-06 17:43:24 -0300

    WIP: Try implement philo struct.  Changes to be committed: 	new file:   philo/src/routine/ft_monitor.c 	new file:   philo/src/routine/ft_monitor.h 	new file:   philo/src/routine/ft_philo.c 	new file:   philo/src/routine/ft_philo.h 	modified:   philo/src/routine/ft_routine.h

--- philo/src/routine/ft_routine.h
+++ philo/src/routine/ft_routine.h
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 04:52:22 by dande-je          #+#    #+#             */
-/*   Updated: 2024/12/01 01:06:54 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/12/05 11:48:24 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,20 @@ enum e_scale_constants
 	MS_PER_SEC = (int)1e3,
 };
 
-typedef struct s_info		t_info;
-struct s_info
+typedef struct s_data		t_data;
+struct s_data
 {
-	int				number_of_philosophers;
-	t_pthread_mutex	time_to_die;
-	t_pthread_mutex	time_to_eat;
-	t_pthread_mutex	time_to_sleep;
-	t_pthread_mutex	number_of_times_each_philosopher_must_eat;
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
 };
 
 typedef struct s_routine	t_routine;
 struct s_routine
 {
-	t_info	data;
+	t_data	data;
 };
 
 t_routine	*ft_routine(void);
